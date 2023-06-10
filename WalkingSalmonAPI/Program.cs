960199bc711359131ed730cd900bcce8bee49308
hoang bao <baohoang.7882@hotmail.com>
2023-06-10 14:16:43 +0700

    Add connectionstrings

--- WalkingSalmonAPI/Program.cs
+++ WalkingSalmonAPI/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using Repository;
 using Repository.Implementation;
+using Repository.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<EXE221Context>(
+  options => options.UseSqlServer("name=ConnectionStrings:EXE221"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
